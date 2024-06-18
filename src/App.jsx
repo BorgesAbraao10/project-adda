a5e81d6580200d21869c144d2f9e6d71a967d440
BorgesAbraao10 <abraao10clear@gmail.com>
2024-06-18 12:44:29 -0300

    Last update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import { Navbar }from './components/Navbar/Navbar'
 import './styles/App.css';
 import { Article } from "./components/article/Article";
 
+import imac from './assets/images/imac.JPG';
+import ipod from './assets/images/ipod.JPG';
+import macbook from './assets/images/macbook.JPG';
+import ipad from './assets/images/ipad-retina.JPG';
+
+
 
 // Componente em classe é uma classe que herda uma classe Component do React e 
 // retorna HTML dentro do método render.
@@ -16,10 +22,32 @@ class App extends React.Component {
             <Navbar />
 
             <section id="articles">
-            <Article title='Exemplo 1' />
-            <Article title='Exemplo 2' />
-            <Article title='Exemplo 3' />
-            <Article title='Exemplo 4' />
+
+            <Article title='Design Dashbords' 
+            provider= "NASA" 
+            descriptions= '...' 
+            thumbinail={ipad}/>
+        
+            
+
+                   
+            <Article title='Vibrant Portaits of 2022'
+            provider= 'Space News'
+            descriptions= '...'
+            thumbinail={imac} />
+
+        
+
+            <Article title='36 Days of Malaya type'
+            provider= 'Spacefligtht Now' 
+            descriptions= '...'
+            thumbinail={ipod} />
+
+            <Article title='Design Dashbords' 
+            provider= "NASA" 
+            descriptions= "..."
+            thumbinail={macbook} />
+
             </section>
         </>
         );   
--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 
-import ipadretina from '../../assets/ipad-retina.JPG';
-
 import './styles.css';
 
 export class Article extends React.Component {
     render() {
         return (
             <article id='article'>
-                <img src={ipadretina} alt="" />
+                <img src={this.props.thumbinail} alt={this.props.title} />
 
                 <div className='article-infos'>
                     <h2>{this.props.title}</h2>
-                    <h3>Nasa</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis porro dicta, error neque sapiente facere praesentium. Officiis laborum alias ratione blanditiis voluptatum minima neque architecto tenetur. Quod est repellat reprehenderit!</p>
+                    <h3>{this.props.provider}</h3>
+                    <p>{this.props.descriptions}</p>
                 </div>
             </article>
         )
